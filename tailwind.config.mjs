4027d3c94ebc46a8142270dc2751fd37b8b21b19
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-06-12 17:22:53 +0530

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "nav-black": "#222222",
+      },
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
