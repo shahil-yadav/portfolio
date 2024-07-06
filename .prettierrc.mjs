d849b006860478387bd9756f1dedcdcee8d80c44
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-07-06 20:36:34 +0530

    Added Experience + Project section

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,5 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
 };
