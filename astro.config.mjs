754b79f0132c3d1acb197b910c491f46e41a4701
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-07-07 16:08:01 +0530

    Removed base option + rectified image links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: "https://shahil-yadav.github.io",
-  base: "/portfolio",
-});
+  site: "https://shahil-yadav.github.io"
+})
