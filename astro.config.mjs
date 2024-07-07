dafb0beba35c1f84b225886bc548f028504cd283
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-07-07 16:51:01 +0530

    deleted config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  site: "https://shahil-yadav.github.io"
+  integrations: [react(), tailwind()]
 })
