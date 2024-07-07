43c921c6aa648bda56da230937f00ce3e7e1fa63
Shahil Yadav <144932369+shahil-yadav@users.noreply.github.com>
2024-07-07 12:29:58 +0530

    Added skills + Experience section

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,4 +2,6 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-};
+  semi: false,
+  trailingComma: "none"
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://shahil-yadav.github.io",
+  base: "/portfolio",
 });
